3796257e21b264da30408a94994b4dca7981f184
mateusdreher <mateusdreher.dev@gmail.com>
2022-02-04 14:44:42 -0300

    Correct room model

--- src/models/room.model.ts
+++ src/models/room.model.ts
@@ -1,10 +1,7 @@
 import mongoose, { mongo, Schema } from 'mongoose';
 
 const roomSchema = new Schema({
-    meessage: String,
     name: String,
-    send_at: Date,
-    room: String
 });
 
 const roomModel = mongoose.model('rooms', roomSchema);
