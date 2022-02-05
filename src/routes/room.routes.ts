bbac4da7ba369873fd2cb802c735eb2b7bb2241e
mateusdreher <mateusdreher.dev@gmail.com>
2022-02-05 02:26:21 -0300

    Add middleware

--- src/routes/room.routes.ts
+++ src/routes/room.routes.ts
@@ -1,13 +1,14 @@
 import { RoomController } from "@controllers/room.controller";
 import { Router, Request, Response } from "express";
+import { AuthMiddleware } from "middlewares/auth.middleware";
 
 const roomRouter = Router();
 
-roomRouter.post('/', (request: Request, response: Response) => {
+roomRouter.post('/', new AuthMiddleware().authenticate, (request: Request, response: Response) => {
     return new RoomController().create(request, response);
 });
 
-roomRouter.get('/all', (request: Request, response: Response) => {
+roomRouter.get('/all', new AuthMiddleware().authenticate, (request: Request, response: Response) => {
     return new RoomController().getAll(request, response);
 });
 
